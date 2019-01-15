8c001d2c0cc0a4defeac7c95174ed1ce0ace2e8a
kj415j45 <kj415j45@gmail.com>
2019-01-15 15:23:14 +0800

    Initial

---
+++ Search.php
@@ -0,0 +1,76 @@
+<?php
+namespace kjBotModule\kj415j45\pixiv;
+
+use kjBot\Framework\Module;
+use kjBot\Framework\Message;
+use kjBot\Framework\Event\MessageEvent;
+
+class Search extends Module{
+    public function process(array $args, MessageEvent $event): Message
+    {
+        $index = 1;
+        do{
+            $arg = $args[$index++];
+            switch($arg){
+                case '-':
+                    $target = $args[$index++];
+                    break;
+                case '-page':
+                    $page = $args[$index++];
+                    break;  
+                case '-mode':
+                    $mode = $args[$index++];
+                    break;
+                case '-like':
+                    $word.= $args[$index++].urlencode('users入り ');
+                    break;
+                default:
+                    $word.= $arg??'';
+            }
+        }while($arg!==NULL);
+
+        if($event->fromGroup())$mode = 'safe';
+        $webStr = 'https://www.pixiv.net/search.php?type=illust'
+            .'&p='.@$page
+            .'&mode='.strtolower(@$mode)
+            .'&word='.$word??q('请提供关键词')
+        ;
+
+        Utils::Init(\Config('pixivSession'));
+
+        $web = file_get_contents($webStr, false, stream_context_create(Utils::$pixivCookieHeader));
+        if($web===false)q('无法打开 Pixiv');
+
+        preg_match('/data-items="([^"]*)"/', $web, $match);
+        preg_match('/<span class="count-badge">(\d+)件/', $web, $count);
+
+        $json = html_entity_decode($match[1]);
+        
+        if($json == '[]' || $json == '')q('没有结果');
+        
+        $result = json_decode($json);
+        
+        if(isset($target) && 1<=$target && $target<=count($result)){
+            $index = $target-1;
+        }else{
+            $index = rand(0, count($result)-1);
+        }
+        
+        $pixiv = $result[$index++];
+        $pixiv = Utils::GetIllustInfoByID($pixiv->illustId);
+        $tags = Utils::GetIllustTagsFromPixivJSON($pixiv);
+        $pixiv->illustComment = strip_tags(str_replace('<br />', "\n", $pixiv->illustComment));
+        $msg=<<<EOT
+该关键字共有 {$count[1]} 幅作品，这是第 {$page} 页第 {$index} 幅
+插画ID：{$pixiv->illustId} 共 {$pixiv->pageCount} P
+画师ID：{$pixiv->userId}
+标签：{$tags}
+收藏：{$pixiv->bookmarkCount} 喜欢：{$pixiv->likeCount} 浏览：{$pixiv->viewCount}
+
+{$pixiv->illustTitle}
+{$pixiv->illustComment}
+[CQ:image,file={$pixiv->urls->regular}]
+EOT;
+        return $event->sendBack($msg);
+    }
+}
---
+++ Utils.php
@@ -0,0 +1,39 @@
+<?php
+namespace kjBotModule\kj415j45\pixiv;
+
+class Utils{
+    public static $pixivCookieHeader;
+
+    //cookie至少需要包含phpsessionid和devide_token
+    public static function Init($cookie = NULL){
+        static::$pixivCookieHeader = [
+            "http" => [
+                "header" => 'cookie: '.\Config('pixivCookie')."\n"
+            ]
+        ];
+    }
+
+    public static function GetIllustInfoByID($iID){
+        $web = file_get_contents('https://www.pixiv.net/member_illust.php?mode=medium&illust_id='.$iID, false, stream_context_create(static::$pixivCookieHeader));
+        if($web===false)leave('无法打开 Pixiv');
+    
+        if(!preg_match('/illust:\s?\{\s?'.$iID.':\s?({[\S\s]*}\})/', $web, $result)){
+            leave('没有这张插画');
+        }
+    
+        $pixiv = json_decode($result[1]);
+        setData('test.txt', var_export($pixiv, true));
+        return $pixiv;
+    }
+    
+    public static function GetIllustTagsFromPixivJSON($pixiv){
+        $tags = $pixiv->tags->tags;
+        $tagString = '';
+        foreach($tags as $tag){
+            $tagString.= $tag->tag.' ';
+        }
+        return rtrim($tagString);
+    }
+    
+    
+}
