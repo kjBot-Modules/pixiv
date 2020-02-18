dd7b4a158808da2aac56d3f32ef7e2931f2fa691
kj415j45 <kj415j45@gmail.com>
2020-02-18 09:54:15 +0800

    Add detail

--- Search.php
+++ Search.php
@@ -92,7 +92,7 @@ class Search extends Module{
 [CQ:image,file={$pixiv->urls->regular}]
 EOT;
         Access::Log($this, $event, "Result: {$pixiv->illustId}, Using keywords: {$word}");
-        Access::LogForMe($this, "Result: {$pixiv->illustId}, Using keywords: {$word}");
+        Access::LogForMe($this, "User: {${$event->getId()}}, Result: {$pixiv->illustId}, Using keywords: {$word}");
         return $event->sendBack($msg);
     }
 }
